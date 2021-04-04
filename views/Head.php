00aa9f94d8086c9f9b70a57dafe2baf2ebf4fa94
yulia-merkulova <81841765+yulia-merkulova@users.noreply.github.com>
2021-04-04 18:27:16 -0400

    Create Head.php
    Translating variables and comments into English

---
+++ views/Head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr_CA">
+<head>
+    <!-- meta -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Tp2 Boutique | MVC avec AJAX</title>
+	<meta name="description" content="Tp2 Boutique réalisé par Yulia Merkulova">
+
+	<!-- styles -->
+    <link rel="stylesheet" type="text/css" href="./styles/styles.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    
+	<!-- scripts -->
+	<script type="text/javascript" src="./scripts/Form.js"></script>
+    <script type="text/javascript" src="./scripts/FormValidator.js"></script>
+	<script type="text/javascript" src="./scripts/Product.js"></script>
+	<script type="text/javascript" src="./scripts/Cart.js"></script>
+	<script type="text/javascript" src="./scripts/Filter.js"></script>
+	<script type="text/javascript" src="./scripts/Input.js"></script>
+	<script type="text/javascript" src="./scripts/Order.js"></script>
+	<script type="text/javascript" src="./scripts/ProductList.js"></script>
+	<script type="text/javascript" src="./scripts/classMapping.js"></script>
+	<script type="text/javascript" src="./scripts/script.js"></script>
+</head>
+
+<body>
