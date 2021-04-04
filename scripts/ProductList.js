883a3081b68c82efd2bf27455c34fc5e62dcbb94
yulia-merkulova <81841765+yulia-merkulova@users.noreply.github.com>
2021-04-04 18:26:34 -0400

    Update ProductList.js
    Translating variables and comments into English

--- scripts/ProductList.js
+++ scripts/ProductList.js
@@ -1,11 +1,13 @@
+// The class ProductList is used to display a list of products 
 class ProductList {
     constructor(el) {
         this._el = el;
-        this._elSelect = this._el.querySelector('[data-js-filter]');
+        this._elCart = document.querySelector('[data-js-cart]');
+        this._elNbInCart = document.querySelector('[data-js-nbInCart]');
+        this._elSelect = document.querySelector('[data-js-filter]');
         this._elSubmit = document.querySelector('[data-js-btn]');
-        this._elResults = document.querySelector('[data-js-results]');
-        this._elNbPanier = document.querySelector('[data-js-nbPanier]');
-
+        this._elBtnMore = document.querySelector('[data-js-more]');
+        
         this.init();
     }
 
@@ -16,41 +18,50 @@ class ProductList {
 
             this.showProductsList();
         });
-        
-        //Si la variable existe, afficher le le nombre des produits au panier
-        if (sessionStorage.getItem('panier')) {
-            //Obtenir le contenu de la variable
-            let tableauObjets = JSON.parse(sessionStorage.getItem('panier'));
-            //Afficher le nombre des produits au panier
-            this._elNbPanier.innerHTML = tableauObjets.length;
 
-            document.querySelector('[data-js-panier]').classList.remove('panier--disabled');
+        this._elBtnMore.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            this.showProductsListMore();
+        });
+        
+        // If the variable sessionStorage exists, display the number of products in the cart
+        if (sessionStorage.getItem('cart')) {
+            // Get the contents of the variable
+            let objectsTable = JSON.parse(sessionStorage.getItem('cart'));
+        
+            // Display the number of products in the cart
+            this._elNbInCart.innerHTML = objectsTable.length;
+
+            // The cart is clickable
+            this._elCart.classList.remove('cart--disabled');
         }
 
     }
 
+    // Display a list of products with filter and limit
     showProductsList = () => {
 
-        // Déclaration de l'objet XMLHttpRequest
+        // Declare the object XMLHttpRequest
         var xhr;
         xhr = new XMLHttpRequest();
         
-        // Initialisation de la requète
+        // Initialization of the request
         if (xhr) {	
 
-            // Ouverture de la requète : fichier recherché
-            xhr.open('GET', 'index.php?Produits_AJAX&action=filterListeProduits&filter=' + this._elSelect.value + '&limit=' + sessionStorage.getItem('nb_products'));
+            // Opening of the request : searched file
+            xhr.open('GET', 'index.php?Products_AJAX&action=filterListProducts&filter=' + this._elSelect.value + '&limit=' + sessionStorage.getItem('nb_products'));
 
             xhr.addEventListener('readystatechange', () => {
 
                 if (xhr.readyState === 4) {							
                     if (xhr.status === 200) {
 
-                        // Les données ont été reçues
-                        // Traitement du DOM
-                        this._elResults.innerHTML = xhr.response;
+                        // Receive data from a server 
+                        // Insertion DOM
+                        this._el.innerHTML = xhr.responseText;
 
-                        //Vérifier si tous les produits sont affichés sur le site
+                        // Check if all products are displayed on the site
                         this.checkNbProducts();
 
                     } else if (xhr.status === 404) {
@@ -59,27 +70,66 @@ class ProductList {
                 }
             });
 
-            // Envoi de la requète
             xhr.send();
         }
     }
 
-    //On cache le bouton Voir plus s'il n'y a plus de produits à afficher
+    showProductsListMore = () => {
+        
+        // Declare the object XMLHttpRequest
+        var xhr;
+        xhr = new XMLHttpRequest();
+        
+        // Initialization of the request
+        if (xhr) {	
+
+            // Opening of the request : searched file
+            xhr.open('GET', 'index.php?Products_AJAX&action=displayListProductsMore&limit=' + this.incrementSessionStorage());
+
+            xhr.addEventListener('readystatechange', () => {
+
+                if (xhr.readyState === 4) {							
+                    if (xhr.status === 200) {
+
+                        // Receive data from a server 
+                        // Insertion DOM
+                        this._el.innerHTML = xhr.responseText;
+
+                        // Check if all products are displayed on the site
+                        this.checkNbProducts();
+
+                    } else if (xhr.status === 404) {
+                        console.log('Le fichier appelé dans la méthode open() n’existe pas.');
+                    }
+                }
+            });
+
+            xhr.send();
+        }
+        
+    }
+    
+    incrementSessionStorage = () => {
+        
+        let nbProducts;
+        nbProducts = parseInt(sessionStorage.getItem('nb_products')) + 12;
+        sessionStorage.setItem('nb_products', nbProducts);
+               
+        return nbProducts;
+    }
+
+    // Hide the button Voir Plus if there are no more products to display
     checkNbProducts = () => {
         
-        let elsProduitsAffiches = this._elResults.querySelectorAll('[data-js-component="Product"]');
-        for (let i = 0, l = elsProduitsAffiches.length; i < l; i++) {
-            //Instancier la classe Product sur chaque tuile
-            new Product(elsProduitsAffiches[i]);
+        let elsProducts = this._el.querySelectorAll('[data-js-component="Product"]');
+        for (let i = 0, l = elsProducts.length; i < l; i++) {
+            // Instantiate the Product class on each tile
+            new Product(elsProducts[i]);
         }
-
-        //Instancier la classe sur le bouton
-        let elBtn = this._elResults.querySelector(".btn-more");
-        new ProductListMore(elBtn);
         
-        if (elsProduitsAffiches.length < parseInt(sessionStorage.getItem('nb_products'))) {
-            elBtn.classList.remove('btn-more');
-            elBtn.classList.add('hidden');
+        if (elsProducts.length < parseInt(sessionStorage.getItem('nb_products'))) {
+            this._elBtnMore.classList.remove('btn-more');
+            this._elBtnMore.classList.add('hidden');
         }
 
     }
